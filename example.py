ecca52e5f0b8ca63e1900e470ccebac8543e4dfe
Fadli Maulana <wetmanz23@gmail.com>
2019-02-24 19:46:57 +0700

    Create example.py

---
+++ example.py
@@ -0,0 +1,61 @@
+# Gathering All Post and Comment from Line Official Account
+
+import linelime
+import json
+import threading
+from copy import copy
+
+# Session ID from cookie
+session = "YOUR_SESSION_ID"
+
+# Home ID from URL
+home_id = "_dc-7fplr21FHo7GOzNeg9XSmLgAineSgyAle1sE" # Draft SMS UGM Home ID
+
+config = linelime.TimelineConfig()
+config.set_session(session)
+config.set_home_id(home_id)
+reader = linelime.TimelineReader(config)
+
+feeds = []
+
+threads = []
+
+def get_feed_threading(reader):
+    feed = reader.get_feed()
+    result = {
+        "postId" : feed["postId"],
+        "likeCount" : feed["likeCount"],
+        "content" : feed["content"],
+        "comments" : reader.fetch_comments()
+    }
+    feeds.append(result)
+
+    # Save available data to txt file
+    with open ('Draft_SMS_UGM.txt', 'a+', encoding="utf-8") as f:
+        f.write(json.dumps(result) + "\n")
+
+
+page = 1
+while reader.fetch_page():
+    print("Fetching page " + str(page) + "...")
+    while reader.fetch_feed() != None:
+        threads.append(threading.Thread(target=get_feed_threading, args=(copy(reader),)))
+    if page % 25 == 0:
+        for thread in threads:
+            thread.start()
+        for thread in threads:
+            thread.join()
+        threads = []
+    page += 1
+
+for thread in threads:
+    thread.start()
+for thread in threads:
+    thread.join()
+
+# Save all data to JSON file after completed
+data = {
+    "feeds" : feeds
+}
+with open ('Draft_SMS_UGM.json', 'w+', encoding="utf-8") as f:
+    f.write(json.dumps(data))
